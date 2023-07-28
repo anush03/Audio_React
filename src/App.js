e93978b08e54bcc8d257498e8e3bc8ec4e834b5a
“Anush” <anushshinde03@gmail.com>
2023-07-29 01:37:19 +0530

    Adding  transcription API service

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import useClipboard from "react-use-clipboard";
-import { useState } from "react";
+
 //import AudioDownload from "./components/AudioDownload";
 import AudioAnalyser from "react-audio-analyser";
 
@@ -16,12 +17,26 @@ const App = () => {
   ////////////////////////////////////////////////////////
   const [status, setStatus] = useState("");
   const [audioSrc, setAudioSrc] = useState(null);
-  const [audioType, setAudioType] = useState("audio/wav");
+  const [audioType, setAudioType] = useState("audio/mp3");
+
+  const [activeBtn, setActiveBtn] = useState(false);
+
+  const controlAudio = (status) => {
+    setStatus(status);
+  };
+
+  const changeScheme = (e) => {
+    setAudioType(e.target.value);
+  };
+
+  useEffect(() => {
+    setAudioType("audio/mp3");
+  }, []);
   const audioProps = {
     audioType,
     // audioOptions: {sampleRate: 30000}, // 设置输出音频采样率
     status,
-    audioSrc,
+    // audioSrc,
     timeslice: 1000, // timeslice（https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/start#Parameters）
     startCallback: (e) => {
       console.log("succ start", e);
@@ -30,6 +45,7 @@ const App = () => {
       console.log("succ pause", e);
     },
     stopCallback: (e) => {
+      setAudioSrc(window.URL.createObjectURL(e));
       console.log("succ stop", e);
     },
     onRecordCallback: (e) => {
@@ -40,13 +56,27 @@ const App = () => {
     },
   };
 
+  const downloadAudio = () => {
+    if (audioSrc) {
+      const downloadLink = document.createElement("a");
+      downloadLink.href = audioSrc;
+      downloadLink.download = "recorded_audio.mp4";
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    }
+  };
   ///////////////////////////////////////////////////////
 
   //subscribe to thapa technical for more awesome videos
 
-  const startListening = () =>
+  const startListening = () => {
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
-  const { transcript, browserSupportsSpeechRecognition } =
+    controlAudio("recording");
+    setActiveBtn(false);
+    resetTranscript();
+  };
+  const { transcript, browserSupportsSpeechRecognition, resetTranscript } =
     useSpeechRecognition();
 
   if (!browserSupportsSpeechRecognition) {
@@ -62,9 +92,9 @@ const App = () => {
           A React hook that converts speech from the microphone to text and
           makes it available to your React components.
         </p>
-
+        <AudioAnalyser {...audioProps}></AudioAnalyser>
         <div className="main-content" onClick={() => setTextToCopy(transcript)}>
-          {transcript}
+          {activeBtn && transcript}
         </div>
 
         <div className="btn-style">
@@ -72,10 +102,39 @@ const App = () => {
             {isCopied ? "Copied!" : "Copy to clipboard"}
           </button>
           <button onClick={startListening}>Start Listening</button>
-          <button onClick={SpeechRecognition.stopListening}>
+          <button
+            onClick={() => {
+              SpeechRecognition.stopListening();
+              controlAudio("inactive");
+              setActiveBtn(true);
+              //SpeechRecognition.resetTranscript();
+            }}
+          >
             Stop Listening
           </button>
+
+          {activeBtn && (
+            <button onClick={downloadAudio} disabled={!audioSrc}>
+              Download Audio
+            </button>
+          )}
         </div>
+      </div>
+
+      <div className="btn-box">
+        {/* <button className="btn" onClick={() => controlAudio("recording")}>
+            Start
+          </button>
+          <button className="btn" onClick={() => controlAudio("paused")}>
+            Pause
+          </button>
+          <button className="btn" onClick={() => controlAudio("inactive")}>
+            Stop
+          </button> */}
+
+        {/* <button onClick={downloadAudio} disabled={!audioSrc}>
+            Download Audio
+          </button> */}
       </div>
     </>
   );
