3287220228c5b9c7a17105059fbc69c106655edd
“Anush” <anushshinde03@gmail.com>
2023-07-29 00:59:30 +0530

    creating a repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+import { useState } from "react";
+//import AudioDownload from "./components/AudioDownload";
+import AudioAnalyser from "react-audio-analyser";
 
-function App() {
+const App = () => {
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
+
+  ////////////////////////////////////////////////////////
+  const [status, setStatus] = useState("");
+  const [audioSrc, setAudioSrc] = useState(null);
+  const [audioType, setAudioType] = useState("audio/wav");
+  const audioProps = {
+    audioType,
+    // audioOptions: {sampleRate: 30000}, // 设置输出音频采样率
+    status,
+    audioSrc,
+    timeslice: 1000, // timeslice（https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/start#Parameters）
+    startCallback: (e) => {
+      console.log("succ start", e);
+    },
+    pauseCallback: (e) => {
+      console.log("succ pause", e);
+    },
+    stopCallback: (e) => {
+      console.log("succ stop", e);
+    },
+    onRecordCallback: (e) => {
+      console.log("recording", e);
+    },
+    errorCallback: (err) => {
+      console.log("error", err);
+    },
+  };
+
+  ///////////////////////////////////////////////////////
+
+  //subscribe to thapa technical for more awesome videos
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A React hook that converts speech from the microphone to text and
+          makes it available to your React components.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
